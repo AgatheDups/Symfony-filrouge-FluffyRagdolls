a5b913026cecd5726dd00b2d0d334a5d9f92a52d
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-10-31 14:14:35 +0100

    migrations

---
+++ migrations/Version20241031092204.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241031092204 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD email VARCHAR(255) NOT NULL, ADD is_breeder TINYINT(1) NOT NULL, ADD siret VARCHAR(14) DEFAULT NULL, ADD city VARCHAR(255) NOT NULL, ADD phone_number VARCHAR(50) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user DROP email, DROP is_breeder, DROP siret, DROP city, DROP phone_number');
+    }
+}
