557f0c1c989d79b96c6c3a19845ad3ac685f9c92
AgatheDups <158457323+AgatheDups@users.noreply.github.com>
2024-11-05 16:09:02 +0100

    add condition to addannouncement and findByUserId to announcement

--- src/Controller/AnnouncementController.php
+++ src/Controller/AnnouncementController.php
@@ -5,17 +5,19 @@ namespace App\Controller;
 use App\Entity\Announcement;
 use App\Form\AddAnnouncementType;
 use App\Repository\AnnouncementRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class AnnouncementController extends AbstractController
 {
-    #[Route('/announcement', name: 'app_announcement')]
+    #[Route('/announcement/{id}', name: 'app_announcement')]
     public function announcement(AnnouncementRepository $announcementRepository, int $id): Response
     {
-        $announcements = $announcementRepository ->findAnnouncementsByUserID($id);
+        $announcements = $announcementRepository ->findByUserId($id);
 
         return $this->render('announcement/index.html.twig', [
             'announcements' => $announcements,
@@ -24,13 +26,27 @@ class AnnouncementController extends AbstractController
     }
 
     #[Route('/announcement-create', name: 'app_announcement_create')]
-    public function announcementCreate(Request $request): Response
+    public function announcementCreate(Request $request, EntityManagerInterface $entityManager, Security $security): Response
     {
+        $user = $security->getUser();
+
         $announcement = new Announcement();
         $form = $this->createForm(AddAnnouncementType::class, $announcement);
         $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if ($user) {
+                $announcement->setUser($user);
+            }
+
+            $entityManager->persist($announcement);
+            $entityManager->flush();
+
+            $id = $announcement->getId();
+            return $this->redirect($this->generateUrl('app_announcement', ['id' => $id]));
+        }
         return $this->render('announcement/index-create.html.twig', [
-            'addAnnouncementForm' => $form,
+            'addAnnouncementForm' => $form->createView(),
         ]);
     }
 }
